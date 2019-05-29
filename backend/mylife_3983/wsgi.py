dbeb1704b5bce8da593371e002d7b583b3a7e822
Crowdbotics <team@crowdbotics.com>
2019-05-29 17:14:16 +0000

    initial commit

---
+++ backend/mylife_3983/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mylife_3983 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mylife_3983.settings")
+
+application = get_wsgi_application()
